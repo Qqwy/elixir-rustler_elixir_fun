803ae75713991981cb5da540cf7316bb2568431a
Marten/Qqwy <w-m@wmcode.nl>
2022-05-27 14:16:15 +0200

    Fix usage with timeouts

--- lib/rustler_elixir_fun/internal.ex
+++ lib/rustler_elixir_fun/internal.ex
@@ -11,6 +11,16 @@ defmodule RustlerElixirFun.Internal do
   """
   def apply_elixir_fun(_pid_or_name, _fun, _parameters), do: :erlang.nif_error(:nif_not_loaded)
 
+  @doc """
+  Calls the Rust NIF directly.
+
+  This is mainly useful to simplify testing;
+  in your actual projects, you'll want to call the Rust function directly from your Rust code.
+
+  This variant allows customizing the maximum time we wait for the function to return (to prevent any deadlocks).
+  """
+  def apply_elixir_fun_timeout(_pid_or_name, _fun, _parameters, _timeout_milliseconds), do: :erlang.nif_error(:nif_not_loaded)
+
   @doc false
   # Called internally by the wrapper process
   # once it has finished running the elixir function,
--- native/rustler_elixir_fun_nif/src/lib.rs
+++ native/rustler_elixir_fun_nif/src/lib.rs
@@ -3,6 +3,7 @@ use rustler::error::Error;
 use rustler_stored_term::StoredTerm;
 use rustler_elixir_fun;
 use rustler_elixir_fun::ManualFuture;
+use std::time::Duration;
 
 fn load(_env: Env, _info: Term) -> bool {
     true
@@ -15,16 +16,22 @@ mod atoms {
     }
 }
 
-#[rustler::nif(
-    name = "apply_elixir_fun",
-    schedule = "DirtyCpu"
-)]
+#[rustler::nif(schedule = "DirtyCpu")]
 /// Exposed as NIF for easy testing
 /// But normally, you'd want to call `rustler_elixir_fun::apply_elixir_fun`
 /// from some other Rust code (rather than from Elixir) instead.
-fn apply_elixir_fun_nif<'a>(env: Env<'a>, pid_or_name: Term<'a>, fun: Term<'a>, parameters: Term<'a>) -> Result<Term<'a>, Error> {
+fn apply_elixir_fun<'a>(env: Env<'a>, pid_or_name: Term<'a>, fun: Term<'a>, parameters: Term<'a>) -> Result<Term<'a>, Error> {
     Ok(rustler_elixir_fun::apply_elixir_fun(env, pid_or_name, fun, parameters)?.encode(env))
 }
+
+#[rustler::nif(schedule = "DirtyCpu")]
+/// Exposed as NIF for easy testing
+/// But normally, you'd want to call `rustler_elixir_fun::apply_elixir_fun_timeout`
+/// from some other Rust code (rather than from Elixir) instead.
+fn apply_elixir_fun_timeout<'a>(env: Env<'a>, pid_or_name: Term<'a>, fun: Term<'a>, parameters: Term<'a>, timeout: u64) -> Result<Term<'a>, Error> {
+    Ok(rustler_elixir_fun::apply_elixir_fun_timeout(env, pid_or_name, fun, parameters, Duration::from_millis(timeout))?.encode(env))
+}
+
 
 #[rustler::nif]
 /// Called by the internal Elixir code of this library whenever a function is completed.
@@ -39,4 +46,4 @@ fn fill_future<'a>(result: StoredTerm, raw_future_ptr: usize) {
     unsafe { future_ptr.as_ref().expect("Should be a ManualFuture") }.fill(result);
 }
 
-rustler::init!("Elixir.RustlerElixirFun.Internal", [apply_elixir_fun_nif, fill_future], load = load);
+rustler::init!("Elixir.RustlerElixirFun.Internal", [apply_elixir_fun, apply_elixir_fun_timeout, fill_future], load = load);
